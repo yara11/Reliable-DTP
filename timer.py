4a37f15fa948642bc35804a1f951399e3d10a179
Yara <yara.m.abdullatif@gmail.com>
2018-05-04 17:51:49 +0200

    fixed error in timer

--- timer.py
+++ timer.py
@@ -4,18 +4,16 @@ class timer:
 	#initializing
 	def __init__(self, timeout):
 		self.timeout = timeout
-		self.current_time = time.time()
-		self.stop_time = self.current_time + self.timeout
+		self.start_timer()
 
 
 	#starting timer
 	def start_timer (self):
-		self.current_time = time.time()
-		self.stop_time = self.current_time + self.timeout
+		self.stop_time = time.time() + self.timeout
 
 	#time out
 	def timer_timeout(self):
-		if self.current_time >= self.stop_time:
+		if time.time() >= self.stop_time:
 			return True
 		else:
 			return False
